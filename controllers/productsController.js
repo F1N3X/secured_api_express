27b78207919d60eb4779b7bacbd467922fa32518
F1N3X <quartzman92@gmail.com>
2025-04-15 14:15:34 +0200

    POST products into supabase

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,4 +1,5 @@
 const axios = require("axios");
+const supabase = require('../supabaseClient');
 
 const SHOPIFY_ACCESS_TOKEN = process.env.SHOPIFY_TOKEN; // token d'accès API
 
@@ -28,6 +29,8 @@ exports.getProducts = async (req, res) => {
 exports.createProduct = async (req, res) => {
     const { title, price } = req.body;
 
+    console.log(req.user.id + " a créé un produit");
+
     if (!title || !price) {
         return res.status(400).json({ error: "Le nom et le prix du produit sont requis" });
     }
@@ -53,7 +56,29 @@ exports.createProduct = async (req, res) => {
             }
         );
 
-        res.status(201).json({ message: "Produit créé avec succès", product: response.data.product });
+        const shopifyProduct = response.data.product;
+
+        const { data, error } = await supabase
+        .from('products')
+        .insert([
+            {
+              shopify_id: shopifyProduct.id,
+              created_by: req.user.id,
+              sales_count: 0
+            }
+        ]);
+
+        if (error) {
+        console.error('Erreur Supabase:', error);
+        return res.status(500).json({ error: "Erreur lors de l'enregistrement en base" });
+        }
+
+        res.status(201).json({
+        message: "Produit créé avec succès",
+        product: shopifyProduct,
+        db_entry: data,
+        });
+        
     } catch (error) {
         console.error("Erreur Shopify:", error.response?.data || error.message);
         res.status(500).json({ error: "Erreur lors de la création du produit" });
--- routes/productsRoute.js
+++ routes/productsRoute.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 const { getProducts, createProduct } = require('../controllers/productsController');
 
-router.get('/', getProducts);
-router.post('/', createProduct);
+const authorizer = require('../middlewares/authorizer');
+const checkPermission = require('../middlewares/checkPermission');
+
+router.get('/', authorizer, checkPermission('can_post_products'), getProducts);
+router.post('/',authorizer, checkPermission('can_post_products'), createProduct);
 
 module.exports = router;
